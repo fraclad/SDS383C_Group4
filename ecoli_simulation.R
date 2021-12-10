62246682f4a17079610c811c452c44a795bbaa04
emily13hsiao <38193623+emily13hsiao@users.noreply.github.com>
2021-12-10 16:19:05 -0600

    added leaf dataset as well

--- ecoli_simulation.R
+++ ecoli_simulation.R
@@ -1,9 +1,14 @@
 library(dbscan)
 library(speccalt)
-library(clusterR)
 library(dirichletprocess)
 library(mvnfast)
 library(mclust)
+library(NbClust)
+library(tsne)
+library(scatterplot3d)
+library(ggplot2)
+
+tiff("test.tiff", units="in", width=5, height=5, res=300)
 
 data("iris")
 x <- as.matrix(iris[, 1:4])
@@ -18,6 +23,11 @@ ecoli <- read.table("./data/ecoli.data")
 data <- ecoli[, 2:8]
 # labels
 labels <- ecoli[,9]
+
+# kmeans
+km = kmeans(data, centers = 8)
+km_assignments <- km$cluster
+km_rand <- adjustedRandIndex(labels, km_assignments)
 
 # dbscan
 kNNdistplot(data, k = 8)
@@ -35,8 +45,52 @@ model <- DirichletProcessMvnormal(data)
 
 # fgm
 source("./CODE_original.R")
-fgm <- FG_mixture(data, M=25, Iter=500, alpha=1, mm=10)
-index_fg = rep(1,nrow(data)) # cluster assignments
+fgm <- FG_mixture(data, K = 10, M=25, Iter=500, alpha=1, mm=10)
+index_fg = rep(1,nrow(data)) # cluster assignments 
+# assignments are based on what sphere it is assigned 
+L = ncol(fgm$inclusion_matrix)
+for(j in 1:L) {
+  index_fg[which(fgm$inclusion_matrix[,j]==1)]=j
+}
+fg_rand <- adjustedRandIndex(labels, index_fg)
+
+results <- data.frame(c("k-means", "dbscan", "speccalt", "FG"),
+                      c(km_rand, db_rand, s_rand, fg_rand))
+colnames(results) <- c("Method", "RandIndex")
+order <- c("dbscan", "k-means", "speccalt", "FG")
+
+ggplot() + 
+  geom_col(data=results, aes(x=Method, y=RandIndex)) +
+  scale_x_discrete(limits = order) +
+  ylab("adjusted Rand Index") +
+  ggtitle("e. coli protein clustering")
+
+ggsave("ecoli_bar.jpeg")
+
+leaf <- read.csv("./leaf/leaf.csv")
+
+features <- leaf[, 3:16]
+labels <- leaf[,1]
+
+# k means
+km = kmeans(features, centers = 36)
+km_assignments <- km$cluster
+km_rand <- adjustedRandIndex(labels, km_assignments)
+
+# dbscan
+db <- dbscan(features, eps = 0.3, minPts = 3)
+db_assignments <- db$cluster
+db_rand <- adjustedRandIndex(labels, db_assignments)
+
+# speccalt
+kern <- local.rbfdot(features)
+s <- speccalt(kern) # I think these are the labels of the clusters it assigns
+s_rand <- adjustedRandIndex(labels, s)
+
+# fg mixture
+source("./SDS383c_Group4-main/CODE_original.R")
+fgm <- FG_mixture(data, K = 10, M=25, Iter=500, alpha=1, mm=10)
+index_fg = rep(1,nrow(data)) # cluster assignments 
 # assignments are based on what sphere it is assigned 
 L = ncol(fgm$inclusion_matrix)
 for(j in 1:L) {
@@ -44,3 +98,14 @@ for(j in 1:L) {
 }
 fgm_rand <- adjustedRandIndex(labels, index_fg)
 
+results <- data.frame(c("k-means", "dbscan", "speccalt", "FG"),
+                      c(km_rand, db_rand, s_rand, fgm_rand))
+colnames(results) <- c("Method", "RandIndex")
+order <- c("dbscan", "k-means", "speccalt", "FG")
+
+ggplot() + 
+  geom_col(data=results, aes(x=Method, y=RandIndex)) +
+  scale_x_discrete(limits = order) +
+  ylab("adjusted Rand Index") +
+  ggtitle("leaf dataset clustering")
+ggsave("leaf.jpeg")
